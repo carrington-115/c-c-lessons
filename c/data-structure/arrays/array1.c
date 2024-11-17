9f570a16d11437a4b4348349a32c58f812b787aa
carrington-115 <frumarkcarrington@gmail.com>
2024-11-17 08:16:16 +0530

    learned how to create multiple memory spaces with malloc and manage the different spaces at different intervals

--- c/data-structure/arrays/array1.c
+++ c/data-structure/arrays/array1.c
@@ -11,12 +11,32 @@ int main(void)
     }
     for (int i = 0; i < 3; i++)
     {
-        list[i] = i;
+        list[i] = i + 1;
     }
 
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (i < 3)
+            tmp[i] = list[i];
+        else
+            tmp[i] = 4;
+    }
+
+    free(list);
+    list = tmp;
+
     // printing
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 4; i++)
     {
         printf("%d,\t", list[i]);
     }
+
+    return 0;
 }
